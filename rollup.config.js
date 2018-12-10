e5204d1565cf9709a4aa73acf3512e4b1fe43266
Kerry Russo <kerry.kim.russo@gmail.com>
2018-12-10 01:26:19 +0000

    chore: add build step for umd version

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
-
 export default {
-  external: ['for-each', 'warning', 'has', 'string.prototype.trim'],
-  plugins: [
-    nodeResolve(),
-    commonjs()
-  ]
+  external: ['string.prototype.trim'],
+  plugins: [nodeResolve(), commonjs()],
 };
